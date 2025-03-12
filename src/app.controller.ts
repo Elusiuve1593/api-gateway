98940e5746b6b49aa59045e6d8f54ab05b15cb50
Roma <elusiuve15.09.93@icloud.com>
2025-03-12 13:50:25 +0200

    api-gateway

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateOrderRequest } from './greate-order-request.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createOrderRequest(@Body() body: CreateOrderRequest) {
+    return this.appService.createOrderRequest(body)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './greate-order-request.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('BILLING_SERVICE') private readonly billingClient: ClientKafka,
+  ) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createOrderRequest({ userId, price }: CreateOrderRequest) {
+    this.billingClient.emit(
+      'order_created',
+      new OrderCreatedEvent('123', userId, price),
+    );
   }
 }
